a0a2d0d3eda572e9e0ab788f3375d4bebc784a5b
gagewrye <95107220+gagewrye@users.noreply.github.com>
2024-11-22 18:46:02 -0800

    Formatting

--- Super Resolution/Schrodinger Diffusion/models/models.py
+++ Super Resolution/Schrodinger Diffusion/models/models.py
@@ -1,26 +1,13 @@
-import math
+import os
 import torch
 import torch.nn as nn
 import numpy as np
-from torch.nn import Conv2d, Module
-from torchgeo.models import resnet18, get_weight
-from torchvision.models.resnet import ResNet
-from typing import Optional
+from torch.nn import Module
 from i2sb.diffusion import Diffusion
 
-"""
-Pretrained model Weights from SSL4EO-12 dataset
-@ https://github.com/zhu-xlab/SSL4EO-S12
+cwd = os.path.dirname(os.path.realpath(__file__))
 
-Imported using torchgeo
-@ https://torchgeo.readthedocs.io/en/stable/api/models.html
-
-SWIN high resolution aerial imagery model from Satlas
-@ https://github.com/allenai/satlas
-
-New weights can be imported from torchgeo using:
-torchgeo.models.get_weight("ResNet50_Weights.SENTINEL2_ALL_MOCO")
-"""
+from ....DroneClassification.models import ResNet_UNet
 
 """
    _____   _                       _    __   _                     
@@ -30,132 +17,9 @@ torchgeo.models.get_weight("ResNet50_Weights.SENTINEL2_ALL_MOCO")
  | |____  | | | (_| | \__ \ \__ \ | | | |   | | |  __/ | |    \__ \
   \_____| |_|  \__,_| |___/ |___/ |_| |_|   |_|  \___| |_|    |___/
                                                                    
-"""                                                             
-class ResNet_UNet(Module):
-    """
-    UNet architecture with ResNet encoder.
-    
-    Default ResNet is a ResNet18 trained on Sentinel-2 3 channel RGB satellite imagery.
-    """
-    def __init__(self, ResNet: Optional[ResNet] = None, input_image_size=256):
-        super(ResNet_UNet, self).__init__()
-        if ResNet is None:
-            ResNet = resnet18(
-                weights=get_weight("ResNet18_Weights.SENTINEL2_RGB_SECO")
-            )
-        self.input_image_size = input_image_size
-        for param in ResNet.parameters():
-            param.requires_grad = False
-        
-        self.layer1 = nn.Sequential(
-            ResNet.conv1,
-            ResNet.bn1,
-            nn.ReLU(),
-            ResNet.maxpool,
-            ResNet.layer1,
-        )
-        self.layer2 = ResNet.layer2
-        self.layer3 = ResNet.layer3
-        self.layer4 = ResNet.layer4
-
-        dummy_input = torch.randn(1, 3, input_image_size, input_image_size)
-        x = self.layer1(dummy_input)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        
-        # Define feature dimensions
-        feature_dim = x.shape[1]
-        half_dim = feature_dim // 2
-        quarter_dim = feature_dim // 4
-        eighth_dim = feature_dim // 8
-        sixteenth_dim = feature_dim // 16
-        
-        # Center
-        self.center = Decoder(feature_dim, int(feature_dim // 1.5), half_dim)
-
-        # Skip connections
-        self.skip_conv1 = Conv2d(half_dim, half_dim, kernel_size=1)
-        self.skip_conv2 = Conv2d(quarter_dim, quarter_dim, kernel_size=1)
-        self.skip_conv3 = Conv2d(eighth_dim, eighth_dim, kernel_size=1)
-
-        #decoder
-        self.decoder1 = Decoder(feature_dim, half_dim, quarter_dim)
-        self.decoder2 = Decoder(half_dim, quarter_dim, eighth_dim)
-        
-        self.classification_head = nn.Sequential(
-            Upsample(quarter_dim, eighth_dim, sixteenth_dim),
-            Conv2d(sixteenth_dim, 1, kernel_size=2, padding=1),
-            nn.Upsample(
-              size=(input_image_size, input_image_size),
-              mode="bilinear",
-              align_corners=False,
-            ),
-            Conv2d(1, 1, kernel_size=3, padding=1) # smooth output
-        )
-
-    def forward(self, image, timestep=None):
-        if len(image.shape) == 3:
-            image = image.unsqueeze(0)
-        image = image[:, :3, :, :]
-
-        if timestep:
-            # Encode with timestep embeddings
-            x1 = self.add_timestep_embedding(self.layer1(image), timestep)
-            x2 = self.add_timestep_embedding(self.layer2(x1), timestep)
-            x3 = self.add_timestep_embedding(self.layer3(x2), timestep)
-            x4 = self.add_timestep_embedding(self.layer4(x3), timestep)
-        else:
-            # Encode
-            x1 = self.layer1(image)
-            x2 = self.layer2(x1)
-            x3 = self.layer3(x2)
-            x4 = self.layer4(x3)
-      
-        # Center
-        x = self.center(x4)
-
-        # Decode
-        x = torch.cat((x, self.skip_conv1(x3)), dim=1)
-        x = self.decoder1(x)
-        x = torch.cat((x, self.skip_conv2(x2)), dim=1)
-        x = self.decoder2(x)
-        x = torch.cat((x, self.skip_conv3(x1)), dim=1)
-        x = self.classification_head(x)
-
-        return x
-
-class ResNet_FC(Module):
-    """
-    ResNet with Fully Connected output layer.
-    """
-    def __init__(self, ResNet : Optional[ResNet] = None, num_classes=1, input_image_size=128):
-        super(ResNet_FC, self).__init__()
-        self.num_classes = num_classes
-        self.input_image_size = input_image_size
-        if ResNet is None:
-            ResNet = resnet18(
-                weights=get_weight("ResNet18_Weights.SENTINEL2_RGB_SECO")
-            )
-        ResNet.fc = nn.Identity()
-        self.resnet = ResNet
-
-        dummy_input = torch.randn(1, 3, input_image_size, input_image_size)
-        features = ResNet(dummy_input)
-        feature_dim = features.shape[1]
-        # Add a fully connected layer for binary segmentation
-        self.classification_head = nn.Linear(feature_dim, input_image_size*input_image_size * num_classes)
-
-    def forward(self, image):
-        image = image[:, :3, :, :]
-        x = self.resnet(image)
-        x = x.flatten(start_dim=1)
-        x = self.classification_head(x)
-        x = x.view(-1, self.num_classes, self.input_image_size, self.input_image_size)
-        return x
-    
+"""       
 class ResNet_UNet_Diffusion(Module):
-    def __init__(self, unet: Optional[ResNet_UNet] = None, num_timesteps=1000, opt=None):
+    def __init__(self, unet = None, num_timesteps=1000, opt=None):
         super(ResNet_UNet_Diffusion, self).__init__()
         if unet is None:
             unet = ResNet_UNet()
@@ -228,6 +92,7 @@ class ResNet_UNet_Diffusion(Module):
             x = self.classification_head(x)
 
         return x
+
 """
   _    _          _                             
  | |  | |        | |                            
@@ -239,83 +104,6 @@ class ResNet_UNet_Diffusion(Module):
                      |_|                        
 """
 
-class SegmentModelWrapper(Module):
-    def __init__(self, model: nn.Module, threshold=0.5):
-        super(SegmentModelWrapper, self).__init__()
-        self.model = model
-        self.model.eval()
-        self.threshold = threshold
-        
-        # Standard mean and std values for ResNet
-        mean = [0.485, 0.456, 0.406]
-        std = [0.229, 0.224, 0.225]
-
-        # Convert mean and std to tensors with shape [C, 1, 1]
-        self.mean_tensor = torch.tensor(mean, dtype=torch.float32).view(1, 3, 1, 1)
-        self.std_tensor = torch.tensor(std, dtype=torch.float32).view(1, 3, 1, 1)
-
-    
-    def forward(self, image: np.ndarray):
-        """
-        Expects a numpy array of dimensions CxHxW.
-         
-        It can also accept batched images of size BxCxHxW.
-        """
-        image = torch.tensor(image, dtype=torch.float32)
-        image.div_(255.0)
-        if len(image.shape) == 3:
-            image = image.unsqueeze(0)
-        
-        image = (image - self.mean_tensor) / self.std_tensor
-        
-        out = self.model.forward(image)
-
-        out = torch.sigmoid(out)
-
-        return (out > self.threshold).to(torch.uint8)
-
-class Upsample(Module):
-    """
-    Helper class for the UNet architecture.
-    Uses convolutional layers to upsample the input.
-    """
-    def __init__(self, in_channels, mid_channels, out_channels):
-        super(Upsample, self).__init__()
-        self.upsampler = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=2, stride=2)
-        )
-    
-    def forward(self, x) -> torch.Tensor:
-        return self.upsampler(x)
-
-class Decoder(Module):
-    """
-    Helper class for the UNet architecture.
-    Uses convolutional layers to upsample the input.
-    Includes dropout layer to prevent overfitting.
-    """
-    def __init__(self, in_channels: int, mid_channels: int, out_channels: int):
-        super(Decoder, self).__init__()
-        self.decoder = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=2, stride=2)
-        )
-
-    def forward(self, x) -> torch.Tensor:
-        return self.decoder(x)
-    
 class DiffusionLayer(nn.Module):
     def __init__(self, latent_dim, time_embedding_dim):
         super(DiffusionLayer, self).__init__()
