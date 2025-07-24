29c06402b2e0907eb5ce817ca07f0c163e508686
Gage Wrye <95107220+gagewrye@users.noreply.github.com>
2025-07-24 16:18:53 -0700

    Added NoSkip UNet and I2I Diffusion

--- DroneClassification/models/__init__.py
+++ DroneClassification/models/__init__.py
@@ -1,2 +1,2 @@
-from .models import ResNet_UNet, ResNet_FC, SegmentModelWrapper
+from .models import ResNet_UNet, ResNet_FC, SegmentModelWrapper, ResNet_UNet_NoSkip
 from .loss import JaccardLoss, FocalLoss, DistanceCountLoss, LandmassLoss
